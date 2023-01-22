8591935dd23da4f516d4a4d26755936c01aace9c
lemstanislav5 <lemstanislav5@gmail.com>
2023-01-22 22:28:50 +0300

    add request

--- src/api/request.js
+++ src/api/request.js
@@ -1,21 +1,11 @@
 import axios from "axios";
 const URL = 'http://212.193.48.242:4000';
 
-export const sendPhoneNumber = (phone) => {
-  return axios.post(URL + '/api/authentication', {
-    type: 'authentication', phone
-  })
-  .then((res) => {
-    if(res.type === "authentication" && res.status === "enter_code") {
-      return true;
-    } else {
-      return false;
-    }
-
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+export const sendPhoneNumber = async (phone) => {
+  console.log(URL + '/api/authentication')
+  const result = await axios.post('http://212.193.48.242:4000/api/authentication', { type: 'authentication', phone });
+  console.log(result.data)
+  if(result.data !== undefined && result.data.status === "enter_code") return true;
 }
 
   //https://63c9375b904f040a9658e4e3.mockapi.io/api/get/uesrs
--- src/screens/Authentication.jsx
+++ src/screens/Authentication.jsx
@@ -1,3 +1,4 @@
+//!ИСПОЛЬЗОВАТЬ ОДНУ И ТУЖЕ ФОРМУ
 import * as React from 'react';
 import { ActivityIndicator, View, SafeAreaView, StyleSheet, TextInput,  Pressable, Alert, Text } from 'react-native';
 import { authThunk } from '../redux/authenticationReducer';
@@ -5,36 +6,53 @@ import { connect } from 'react-redux';
 import { getAuthSelector } from '../redux/selectrors';
 
 const Auth = (props) => {
-  const { auth, sendPhone } = props;
-  const [phone, onChangeText] = React.useState('+7');
+  const { auth, sendPhoneAuth } = props;
+  const [phone, onChangePhone] = React.useState('+7');
+  const [code, onChangeCode] = React.useState('* * * *');
   const [indicator, onIndicator] = React.useState(false);
-  console.log(indicator)
-  const enterNumber = num => {
-    if(phone.length < 2) {
-      onChangeText('+7');
-    } else {
-      onChangeText(num);
-    }
-  }
-  const send = phone => {
+  console.log('auth:', auth)
+  React.useEffect(() => {
+    console.log('auth:', auth)
+    onIndicator(false);
+ }, [auth]);
+  const enterNumber = num => (phone.length < 2) ? onChangePhone('+7') : onChangePhone(num);
+  const sendPhone = () => {
     onIndicator(true);
-    sendPhone(phone);
+    sendPhoneAuth(phone);
   }
 
   return (
     <View>
         <SafeAreaView style={styles.container}>
         { (indicator)? <ActivityIndicator size="large" color="#00ff00" /> : '' }
-          <TextInput
-            style={styles.input}
-            onChangeText={enterNumber}
-            value={phone}
-            keyboardType='number-pad'
-            textAlign={'center'}
-          />
-          <Pressable style={styles.button} onPress={send}>
-            <Text style={styles.text}>{'SEND'}</Text>
-          </Pressable>
+        {
+          auth ?
+            <>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChangeCode}
+                value={code}
+                keyboardType='number-pad'
+                textAlign={'center'}
+              />
+              <Pressable style={styles.button}>
+                <Text style={styles.text}>{'SEND CODE'}</Text>
+              </Pressable>
+            </>
+          :
+            <>
+              <TextInput
+                style={styles.input}
+                onChangeText={enterNumber}
+                value={phone}
+                keyboardType='number-pad'
+                textAlign={'center'}
+              />
+              <Pressable style={styles.button} onPress={sendPhone}>
+                <Text style={styles.text}>{'SEND PHONE'}</Text>
+              </Pressable>
+            </>
+        }
         </SafeAreaView>
       </View>
     
@@ -84,7 +102,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    sendPhone: (phone) => dispatch(authThunk(phone))
+    sendPhoneAuth: (phone) => dispatch(authThunk(phone))
   };
 };
 
